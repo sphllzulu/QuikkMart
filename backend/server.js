62daef738d16e5904884c5136258f8385befe768
Siphelele <sphllzulu@gmail.com>
2024-11-05 16:35:15 +0200

    changed some cors issues

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ const PORT = process.env.PORT || 8000;
 const server = express();
 const router = express.Router();
 
-server.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    credentials: true
-}));
+server.use(cors());
 
 server.use(express.json());
 
